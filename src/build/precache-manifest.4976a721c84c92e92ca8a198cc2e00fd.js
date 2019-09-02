b97e5251c8adc543289ba1e852c0f946135db70b
Paul Petrisor <petrisorpaulrazvan@gmail.com>
2019-09-02 22:13:44 +0100

    First Commit

---
+++ src/build/precache-manifest.4976a721c84c92e92ca8a198cc2e00fd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ee64d7608e4a1d49c1748eeaf204ea3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fcc42a76d5f052c436e4",
+    "url": "/static/css/main.73ff5b42.chunk.css"
+  },
+  {
+    "revision": "38c623c29afa9a3ce13e",
+    "url": "/static/js/2.c3086949.chunk.js"
+  },
+  {
+    "revision": "fcc42a76d5f052c436e4",
+    "url": "/static/js/main.32175ef4.chunk.js"
+  },
+  {
+    "revision": "c112b2575f81d6c2273b",
+    "url": "/static/js/runtime~main.ba433cf4.js"
+  }
+]);
